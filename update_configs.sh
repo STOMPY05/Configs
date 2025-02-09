5472b2379ecdb7a3af2a8fa42679ce500b5cfec6
STOMPY05 <sj.williams06@googlemail.com>
2025-02-09 12:28:30 -0300

    Update update_configs.sh

--- update_configs.sh
+++ update_configs.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+GITHUB_CONFIG_DIR="$HOME/Git/Config_Files"
+LOCAL_CONFIG_DIR="$HOME/.config"
+
+set -e
+
+# Iterate Over All Files In The GitHub Config Directory
+find "$GITHUB_CONFIG_DIR" -type f | while read -r file; do
+  # Get The Relative Path Of The File
+  relative_path="${file#$GITHUB_CONFIG_DIR/}"
+  
+  # Create The Destination Directory In "~/.config" If It Doesn't Exist
+  mkdir -p "$LOCAL_CONFIG_DIR/$(dirname "$relative_path")"
+  
+  # Delete The Existing Config File If It Exists
+  if [ -e "$LOCAL_CONFIG_DIR/$relative_path" ]; then
+    rm -f "$LOCAL_CONFIG_DIR/$relative_path"
+  fi
+
+  # Create The Symbolic Link, Overwriting It If It Exists
+  ln -sf "$file" "$LOCAL_CONFIG_DIR/$relative_path"
+done
+
+echo "All Files Have Been Sym-Linked From $GITHUB_CONFIG_DIR to $LOCAL_CONFIG_DIR!"
